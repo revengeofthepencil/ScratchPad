e3880efe00797d7eb430bdac261fd3c60dc32952
Alex Walker <revengeofthepencil@gmail.com>
2016-07-13 13:45:11 -0700

    solve palindrome problem

--- src/permutePalindrome/PermutationPal.java
+++ src/permutePalindrome/PermutationPal.java
@@ -1,11 +1,12 @@
 package permutePalindrome;
 
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by alexwalker on 7/13/16.
 
- Write an efficient function that checks whether any permutation ↴ of an input string is a palindrome ↴ .
+ Write an efficient function that checks whether any permutation of an input string is a palindrome .
  Examples:
 
  "civic" should return True
@@ -15,25 +16,44 @@ import java.util.ArrayList;
 
  */
 public class PermutationPal {
-    public boolean checkForPal(String palCheck) {
-        int palCheckLength = palCheck.length();
 
-        for (int i = 0; i < palCheckLength; i++) {
-            int endIdx = palCheckLength - i - 1;
-            char c1 = palCheck.charAt(i);
-            char c2 = palCheck.charAt(endIdx);
-            if (c1 != c2) {
-                return false;
+    public boolean isPalPermute(String permuteString) {
+        permuteString = permuteString.toLowerCase().replaceAll(" ", "");
+        int allowedUneven = permuteString.length() % 2 == 0 ? 0 : 1;
+
+        System.out.println("string = '" + permuteString + "', length = '" + permuteString.length() + "', allowedUneven = " + allowedUneven);
+
+        Map<Character, Integer> charCounts = new HashMap<Character, Integer>();
+        for(char c : permuteString.toCharArray()) {
+            if (charCounts.containsKey(c)) {
+                charCounts.put(c, charCounts.get(c) + 1);
+            } else {
+                charCounts.put(c, 1);
             }
         }
-        return true;
+
+        int foundUneven = 0;
+        for (Map.Entry<Character, Integer> entry : charCounts.entrySet()) {
+            if (entry.getValue() % 2 != 0) {
+                foundUneven++;
+            }
+
+            if (foundUneven > allowedUneven) {
+                break;
+            }
+        }
+
+        return  foundUneven == allowedUneven;
     }
+
 
     public static void main(String[] args) {
         PermutationPal pp = new PermutationPal();
-        String palsToCheck[] = {"civic","ivicc","civil", "livci"};
+        String palsToCheck[] = {"civic", "ivicc","civil", "livci", "civici", "a man a plan a canal panama",
+                "a man a plan panama a canal", "a panamas man a plan  a canal"};
         for (String palCheck : palsToCheck) {
-            boolean isPal = pp.checkForPal(palCheck);
+            System.out.println("\nchecking '" + palCheck + "' ....");
+            boolean isPal = pp.isPalPermute(palCheck);
             System.out.println("'" + palCheck + "' = " + isPal);
         }
 
